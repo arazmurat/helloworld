2e5d0749763b9a737ff2e3418141d2d5c12cd94c
Arazmyrat <arazmirat@icloud.com>
2021-03-28 18:19:40 +0400

    add propTypes

--- src/components/Blogitem.js
+++ src/components/Blogitem.js
@@ -1,20 +1,35 @@
 //CREATE WITH rcc SNIPPET
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 export default class BlogItem extends Component {
-    render() {
-        console.log(this.props)
-        const {blogMessage,nowNmbr} =this.props
-        return (
-            <div>
-                <h1>{this.props.title}</h1>
-                <p>{this.props.description}</p>
-                <h3>{this.props.dataXYZ}</h3>
-                {/* <h4>{this.props.blogMessage}</h4>
+  /**
+   * user ={name:"Tommy",age:12}
+   */
+
+  static propTypes = {
+    title: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+    //description: PropTypes.string.isRequired,
+    //blogMessage: PropTypes.array,
+    //nowNmbr: PropTypes.any.isRequired,
+    //dataXYZ: PropTypes.string
+    //user.PropTypes.shape({name:PropTypes.string,age:PropTypes.number.isRequired}).isRequired //user icin gecerli son isRequired
+  };
+  // Default Props
+  static defaultProps = { title: "One Title", description: "One Desc" };
+  render() {
+    console.log(this.props);
+    const { blogMessage, nowNmbr } = this.props;
+    return (
+      <div>
+        <h1>{this.props.title}</h1>
+        <p>{this.props.description}</p>
+        <h3>{this.props.dataXYZ}</h3>
+        {/* <h4>{this.props.blogMessage}</h4>
                 <h4>{this.props.nowNmbr}</h4> */}
-                <h4>{blogMessage}</h4>
-                <h4>{nowNmbr}</h4>
-            </div>
-        )
-    }
+        <h4>{blogMessage}</h4>
+        <h4>{nowNmbr}</h4>
+      </div>
+    );
+  }
 }
