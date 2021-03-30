187d465abd4e59cfbae53fd7fe5ae62854769b8d
Arazmyrat <arazmirat@icloud.com>
2021-03-30 21:45:14 +0400

    add css

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -2,54 +2,34 @@ import React, { Component } from "react";
 import "../Blog.css";
 import BlogItem from "./BlogItem";
 
-
 //class Blog extends React.Component {}
 class Blog extends Component {
-  
-  render() {
+   render() {
     let blogText = "TEXT TEXT TEXT";
-    const blogParaf = (
-      <h2>
-        BLOG NOTES <button>BUTTON</button>
-      </h2>
-    );
+    const blogParaf =<h2>BLOG NOTES <button>BUTTON</button></h2>
     let nowNumber = 123;
-    let blogInfos = [
-      { textHead: "Blog 1 textHead", textDesc: "Blog 1 textDescription ...." },
-      { textHead: "Blog 2", textDesc: "Blog 2 Description ...." },
-      
-      { textHead: "Blog 1 ile Blog2 ve bu yazi ", textDesc: "blogInfos.map den geliyor" },
-    ];
-    
+    let blogInfos = [{textHead:"Blog 1",textDesc:"Blog 1 Description ...."},
+    {textHead:"Blog 2",textDesc:"Blog 2 Description ...."},
+    {textHead:"Blog 3",textDesc:"Blog 3 Description ...."}
+  ,{textHead:"Blog 4",textDesc:"Blog 4 Description ...."}]
+  const myStyle ={color:"red",padding:"10px",backgroundColor:"Blue",fontFamily:"Arial"}
     return (
       <div className="blogTitle">
-        <h1>BLOG COMPONENT</h1>
+        <h1 style={myStyle}>BLOG COMPONENT</h1>
         {/* <BlogItem title={blogInfos[0].textHead} description={blogInfos[0].textDesc}/>
         <BlogItem title={blogInfos[1].textHead} description={blogInfos[1].textDesc}/>
         <BlogItem title={blogInfos[2].textHead} description={blogInfos[2].textDesc}/>
         <BlogItem title={blogInfos[3].textHead} description={blogInfos[3].textDesc}/> */}
         
+        {blogInfos.map((currentValue,index)=> 
+        <BlogItem key={index} title={currentValue.textHead} description={currentValue.textDesc}/>)}
 
-        {blogInfos.map((item, index) => (
-          <BlogItem
-            key={index}
-            title={item.textHead}
-            description={item.textDesc}
-          />
-        ))}
 
-        <BlogItem
-          dataXYZ="new h3 text"
-          newData={blogParaf}
-          nowNmbr={nowNumber}
-          blogMessage="Testing with PRPS"
-        />
+        <BlogItem title="Student" dataXYZ="new h3 text" newData={blogParaf} nowNmbr={nowNumber} blogMessage="Testing with PRPS" />
         <p>{blogText}</p>
         <label>{5 / 2 === 2.5 ? "YES" : "NO"}</label>
         {blogParaf}
         {5 + 6}
-        
-
       </div>
     );
   }
--- src/components/Blogitem.js
+++ src/components/Blogitem.js
@@ -1,40 +1,47 @@
 //CREATE WITH rcc SNIPPET
-import React, { Component } from "react";
-import PropTypes from "prop-types";
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 export default class BlogItem extends Component {
     constructor(props){
-        // this not used before super ()
-        super();//if we want we use parametre in super for exm super(props)
-        // usable this keyword after super ()
-        console.log("BlogItem Constructor Method ...","Props ", props)
+        super(props);
+        console.log(props)
+        console.log(this.props)
+
+        
       }
   /**
-   * user ={name:"Tommy",age:12}
+   * user={}
+   * user={age:12}
+   * user={name:"Tommy",age:12}
    */
-
-  static propTypes = {
-    title: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
-    //description: PropTypes.string.isRequired,
-    //blogMessage: PropTypes.array,
-    //nowNmbr: PropTypes.any.isRequired,
-    //dataXYZ: PropTypes.string
-    //user.PropTypes.shape({name:PropTypes.string,age:PropTypes.number.isRequired}).isRequired //user icin gecerli son isRequired
+  
+    static propTypes = {
+    title: PropTypes.oneOfType([PropTypes.string,PropTypes.number]).isRequired,
+    description: PropTypes.string,
+    blogMessage:PropTypes.string,
+    nowNmbr:PropTypes.any,
+    dataXYZ:PropTypes.string
+   // user:PropTypes.shape({name:PropTypes.string,age:PropTypes.number.isRequired}).isRequired
   };
-  // Default Props
-  static defaultProps = { title: "Static default props", description: "One Desc" };
+
+  //Default Props
+  static defaultProps = {description:"One Desc",title:"One Title"}
+
+
   render() {
     //console.log(this.props);
     const { blogMessage, nowNmbr } = this.props;
     return (
       <div>
-        <h1>{this.props.title}</h1>
-        <p>{this.props.description}</p>
+        
         <h3>{this.props.dataXYZ}</h3>
         {/* <h4>{this.props.blogMessage}</h4>
                 <h4>{this.props.nowNmbr}</h4> */}
         <h4>{blogMessage}</h4>
         <h4>{nowNmbr}</h4>
+        <h1>{this.props.title}</h1>
+        <p>{this.props.description}</p>
       </div>
     );
   }
